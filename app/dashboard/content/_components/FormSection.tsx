eaa84f139dca29807d0419bd571e45c4e7a7e09e
mustafa <mustafaalikhan757@gmail.com>
2024-07-05 18:36:20 +0530

    some changes

--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { TEMPLATE } from "../../_components/TemplateListSection";
@@ -10,23 +10,33 @@ import { Loader2Icon } from "lucide-react";
 
 interface PROPS {
   selectedTemplates?: TEMPLATE;
-  userFormInput:any
-  loading:boolean
+  userFormInput: any;
+  loading: boolean;
 }
-function FormSection({ selectedTemplates, userFormInput,loading }: PROPS) {
-    const onSubmit=(e:any)=>{
-      e.preventDefault();
-      userFormInput(formData);
-    }
 
-    const [formData,setFormData] = useState<any>();
-    const handleInputChange=(event:any)=>{
-       const {name,value} = event.target;
-       setFormData({...formData,[name]:value})
-    }
+function FormSection({ selectedTemplates, userFormInput, loading }: PROPS) {
+  const [formData, setFormData] = useState<any>({});
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    userFormInput(formData);
+  };
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className="p-5 shadow-md border rounded-lg bg-white">
-      <Image src={selectedTemplates?.icon} alt="icon" width={70} height={70} />
+      {selectedTemplates?.icon && (
+        <Image
+          src={selectedTemplates.icon}
+          alt="icon"
+          width={70}
+          height={70}
+        />
+      )}
       <h2 className="font-bold text-2xl mb-2 text-primary">
         {selectedTemplates?.name}
       </h2>
@@ -34,18 +44,27 @@ function FormSection({ selectedTemplates, userFormInput,loading }: PROPS) {
 
       <form className="mt-6" onSubmit={onSubmit}>
         {selectedTemplates?.form?.map((item, index) => (
-          <div className="my-2 flex flex-col gap-2 mb-7">
+          <div className="my-2 flex flex-col gap-2 mb-7" key={index}>
             <label className="font-bold">{item.label}</label>
-            {item.field == "input" ? (
-              <Input name={item.name} required={item?.required} onChange={handleInputChange}/>
-            ) : item.field == "textarea" ? (
-              <Textarea name={item.name} required={item?.required} onChange={handleInputChange}/>
+            {item.field === "input" ? (
+              <Input
+                name={item.name}
+                required={item?.required}
+                onChange={handleInputChange}
+              />
+            ) : item.field === "textarea" ? (
+              <Textarea
+                name={item.name}
+                required={item?.required}
+                onChange={handleInputChange}
+              />
             ) : null}
           </div>
         ))}
         <Button type="submit" className="w-full py-6" disabled={loading}>
-          {loading&&<Loader2Icon className="animate-spin"/>}Generate Content
-          </Button>
+          {loading && <Loader2Icon className="animate-spin" />}
+          Generate Content
+        </Button>
       </form>
     </div>
   );

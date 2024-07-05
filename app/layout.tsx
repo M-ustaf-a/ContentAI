ea69f3ef6bb3874396b52b6333cdd0e452a810b1
mustafa <mustafaalikhan757@gmail.com>
2024-07-05 18:58:24 +0530

    Add Some code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Outfit({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <SpeedInsights/>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>

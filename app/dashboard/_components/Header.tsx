07a56b7d4310b34adf91182062dc42b2a63eb139
mustafa <mustafaalikhan757@gmail.com>
2024-07-06 00:21:32 +0530

    Some changes

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,15 +1,27 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { Search } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className="p-5 shadow-sm  flex justify-between ">
-      <div className="flex gap-2 items-center p-2  rounded-md max-w-md">
-        {/* <Search/> 
-            {/* <input type="text" placeholder='Search..' className='outline-none bg-transparent'/> */}
+    <div className="p-5 shadow-sm flex justify-between">
+      <div className="flex gap-2 items-center p-2 rounded-md max-w-md">
+        <Search />
+        <input
+          type="text"
+          placeholder="Search.."
+          className="outline-none bg-transparent"
+        />
       </div>
       <div className="flex gap-5 items-center">
-        {/* <h1 className='bg-black p-5 rounded-md text-xs text-white px-4 cursor-pointer '>Join Memebership just for $9.99/Month</h1> */}
+        {/* <h1 className='bg-black p-5 rounded-md text-xs text-white px-4 cursor-pointer '>Join Membership just for $9.99/Month</h1> */}
         {/* <div className="md:w-64 hidden md:block fixed"><UserButton/></div> */}
       </div>
     </div>

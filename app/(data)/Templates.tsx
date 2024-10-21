0d29bf14f83e9f9ad46cd21ec4bd112a17dcdab5
mustafa <mustafaalikhan757@gmail.com>
2024-10-21 17:17:20 +0530

    Some changes

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -84,7 +84,15 @@ export default [
     category: "ssc exam",
     slug: "vocabulary",
     aiPrompt:
-      "Generate almost 200 vocabulary based on exam and give output in rich text editor format with large font"
+      "Generate almost 200 vocabulary based on exam and give output in rich text editor format with large font",
+    form: [
+      {
+        label: "Exam name",
+        field: "input",
+        name: "examName",
+        required: true
+      }
+    ]
   },
   {
     // Tool for generating tax payment processes
